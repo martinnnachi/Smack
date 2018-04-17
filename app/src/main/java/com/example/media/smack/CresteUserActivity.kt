cc119a85106b54034b0f9b7999b33b078e81f69e
Martin Nnachi <mnnachi@gmail.com>
2018-04-17 17:18:39 +0100

    lesson 4

---
+++ app/src/main/java/com/example/media/smack/CresteUserActivity.kt
@@ -0,0 +1,26 @@
+package com.example.media.smack
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CresteUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_creste_user)
+    }
+
+
+    fun generateUserAvatar(view: View){
+
+    }
+
+    fun generateColorClicked(view: View){
+
+    }
+
+    fun createUserClicked(view: View){
+
+    }
+}
--- app/src/main/java/com/example/media/smack/LoginActivity.kt
+++ app/src/main/java/com/example/media/smack/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.media.smack
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -13,9 +14,12 @@ class LoginActivity : AppCompatActivity() {
 
     fun loginCreateUserBtnClicked(view: View) {
 
+        val createUserIntent = Intent(this, CresteUserActivity::class.java)
+        startActivity(createUserIntent)
     }
 
     fun loginLoginBtnClicked(view: View) {
+
 
     }
 }
