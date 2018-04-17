db3732428f0b7fcafe71a6e6ba60502a40860bd8
Martin Nnachi <mnnachi@gmail.com>
2018-04-17 15:29:30 +0100

    lesson 3

--- app/src/main/java/com/example/media/smack/MainActivity.kt
+++ app/src/main/java/com/example/media/smack/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.media.smack
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
@@ -38,6 +39,8 @@ class MainActivity : AppCompatActivity(){
 
     fun loginBtnNavClicked(view: View){
 
+        val loginIntent = Intent(this, LoginActivity::class.java)
+        startActivity(loginIntent)
     }
 
     fun addChannelClicked(view: View){
@@ -45,8 +48,10 @@ class MainActivity : AppCompatActivity(){
     }
 
     fun sendMessageBtnClicked(view: View) {
-        
+
     }
+
+
 
 
 }
