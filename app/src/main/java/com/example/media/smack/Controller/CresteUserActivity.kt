c6e5fe6dc63f8641e1760f67a5f9ab567c7a7f94
Martin Nnachi <mnnachi@gmail.com>
2018-04-20 14:43:33 +0100

    lesson 9

--- app/src/main/java/com/example/media/smack/Controller/CresteUserActivity.kt
+++ app/src/main/java/com/example/media/smack/Controller/CresteUserActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import com.example.media.smack.R
 import com.example.media.smack.Services.AuthService
+import com.example.media.smack.Services.UserDataService
 import kotlinx.android.synthetic.main.activity_creste_user.*
 import java.util.*
 
@@ -50,20 +51,26 @@ class CresteUserActivity : AppCompatActivity() {
     }
 
     fun createUserClicked(view: View) {
+
+        val userName = createUserNameText.text.toString()
         val email = createEmailText.text.toString()
         val password = createPasswordText.text.toString()
 
-        AuthService.registerUser(this, email, password){registerSuccess ->
-            if (registerSuccess){
-                AuthService.loginUser(this, email, password){loginSucces ->
-                    if (loginSucces){
-                        println(AuthService.authToken)
-                        println(AuthService.userEmail)
+        AuthService.registerUser(this, email, password) { registerSuccess ->
+            if (registerSuccess) {
+                AuthService.loginUser(this, email, password) { loginSucces ->
+                    if (loginSucces) {
+                        AuthService.createUser(this, userName, email, userAvatar, avatarColor) { createSuccess ->
+                            if (createSuccess) {
+                                println(UserDataService.avatarName)
+                                println(UserDataService.avatarColor)
+                                println(UserDataService.name)
+                                finish()
+                            }
+                        }
                     }
                 }
             }
-
         }
-
     }
 }
---
+++ app/src/main/java/com/example/media/smack/Services/UserDataService.kt
@@ -0,0 +1,10 @@
+package com.example.media.smack.Services
+
+object UserDataService {
+
+    var id = ""
+    var avatarColor = ""
+    var avatarName = ""
+    var email = ""
+    var name = ""
+}
